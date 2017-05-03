edadda699e408b088ae0bc25b4c96b8f68cb103f
yr180 <yashrao180@gmail.com>
2017-05-03 18:15:43 +0530

    Create Calibration.ino

---
+++ Uno/HX711/Calibration.ino
@@ -0,0 +1,59 @@
+/*
+ This sketch is used to determine the calibration_factor that the main example uses. It also
+ outputs the zero_factor useful for projects that have a permanent mass on the scale in between power cycles.
+
+ Setup your scale and start the sketch WITHOUT a weight on the scale
+ Once readings are displayed place the weight on the scale
+ Press +/- or a/z to adjust the calibration_factor until the output readings match the known weight
+ Use this calibration_factor on the example sketch
+
+ Your calibration factor may be very positive or very negative. It all depends on the setup of your scale system
+ and the direction the sensors deflect from zero state
+ This example code uses bogde's HX711 library, released under a GNU GENERAL PUBLIC LICENSE https://github.com/bogde/HX711
+*/
+
+#include "HX711.h"
+
+#define DOUT  9
+#define CLK  8
+
+HX711 scale(DOUT, CLK);
+
+float calibration_factor = -287500; //Hit and trial
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("HX711 calibration sketch");
+  Serial.println("Remove all weight from scale");
+  Serial.println("After readings begin, place known weight on scale");
+  Serial.println("Press + or a to increase calibration factor");
+  Serial.println("Press - or z to decrease calibration factor");
+
+  scale.set_scale();
+  scale.tare(); //Reset the scale to 0
+
+  long zero_factor = scale.read_average(); //Get a baseline reading
+  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+  Serial.println(zero_factor);
+}
+
+void loop() {
+
+  scale.set_scale(calibration_factor); //Adjust to this calibration factor
+
+  Serial.print("Reading: ");
+  Serial.print((scale.get_units()*0.453592), 3);
+  Serial.print(" kg"); 
+  Serial.print(" calibration_factor: ");
+  Serial.print(calibration_factor);
+  Serial.println();
+
+  if(Serial.available())
+  {
+    char temp = Serial.read();
+    if(temp == '+' || temp == 'a')
+      calibration_factor += 2500;
+    else if(temp == '-' || temp == 'z')
+      calibration_factor -= 2500;
+  }
+}
